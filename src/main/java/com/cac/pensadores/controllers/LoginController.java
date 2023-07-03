6f94b68fc69e17de96092ecc7138dc04cae04e4c
CrisGadea <cristianhernangadea@gmail.com>
2023-07-03 17:58:41 -0300

    first commit

---
+++ src/main/java/com/cac/pensadores/controllers/LoginController.java
@@ -0,0 +1,62 @@
+package com.cac.pensadores.controllers;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+@WebServlet(name = "login", value = "/login")
+public class LoginController extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+        request.getSession().setAttribute("email", request.getParameter("email"));
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>Helllo</h1>");
+        out.println("</body></html>");
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String umail = request.getParameter("email");
+        String upwd = request.getParameter("password");
+        HttpSession session = request.getSession();
+        RequestDispatcher disp = null;
+
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/cac23050?useSSL=false", "root", "12345678");
+            final String QUERY = "select * from users where email = ? and password = ?";
+            PreparedStatement ps = con.prepareStatement(QUERY);
+            ps.setString(1, umail);
+            ps.setString(2, upwd);
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                session.setAttribute("email", rs.getString(1));
+                disp = request.getRequestDispatcher("index.jsp");
+            } else {
+                request.setAttribute("status", "failed");
+                disp = request.getRequestDispatcher("login.jsp");
+            }
+            disp.forward(request, response);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
